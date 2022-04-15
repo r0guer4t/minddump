b9a56e4c8cf9352c9115e435d878a3966912f7ef
r0gue r4t <r0guer4t@dhcppc48.hsd1.pa.comcast.net>
2022-04-15 15:39:47 -0400

    inspired by John Lillys book called programs and metaprograms in the human biocomputer

---
+++ metaprogram.py
@@ -0,0 +1,31 @@
+class biocomputer:
+	def __init__(brain,mind,programs,metaprograms):
+		self.brain = brain
+		self.mind = mind
+		self.programs = programs
+		self.metaprograms = metaprograms 
+	def brain():
+	neurons = GPIO(1.3*10^10)
+	neurons.input(spinal cord)
+	neurons.input(thalamus)
+	neurons.input(cerebellum)
+	neurons.input(medula oblongata)
+	neurons.input(temporal lobe)
+	neurons.input(occipital lobe)
+	neurons.input(parietal lobe)
+	neurons.input(somatosensory cortex)
+	neurons.input(somatomotor cortex)
+	neurons.input(frontal lobe)
+	return brain()
+	def mind():
+	stimulus = brain.input()
+	thoughts = brain + time
+	experiences = time + stimulus
+	knowledge = experiences + thoughts
+	emotions = stimulus * thoughts <= knowledge >= experiences
+	return mind()
+	def programs():
+	program = mind.knowledge + mind.stimulus()
+	return program()
+	def metaprograms():
+	metaprogram = mind.experience <= time
